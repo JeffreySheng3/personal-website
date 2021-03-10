d531ebb178df27ff96eeb1756474b48cfac21063
Jeffrey Sheng <jeffreysheng99@gmail.com>
2021-03-09 19:30:51 -0500

    icons now redirect

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,17 +4,40 @@ import { GrMail } from "react-icons/gr";
 const Body = () => {
    return (
       <h1 className="body">
-         <h3 className="opening">
-            Hey! Thanks for taking the time to get to know me better..
-         </h3>
+         <div>
+            <h3 className="opening">
+               Hey! Thanks for taking the time to get to know me better..
+            </h3>
+         </div>
          <div className="icon-container">
-            <FaGithub className="icon"></FaGithub>
-            <FaLinkedin className="icon"></FaLinkedin>
-            <FaFacebookSquare className="icon"></FaFacebookSquare>
+            <a href="https://github.com/">
+               <button className="icon">
+                  <FaGithub></FaGithub>
+               </button>
+            </a>
+
+            <a href="https://www.linkedin.com">
+               <button className="icon">
+                  <FaLinkedin></FaLinkedin>
+               </button>
+            </a>
+
+            <a href="https://www.facebook.com/">
+               <button className="icon">
+                  <FaFacebookSquare></FaFacebookSquare>
+               </button>
+            </a>
+
+            <a href="https://www.github.com">
+               <button className="icon">
+                  <img src={GrMail} alt="" />
+                  <GrMail></GrMail>
+               </button>
+            </a>
          </div>
-         <div className="icon-container2">
+         {/* <div className="icon-container2">
             <GrMail className="icon"></GrMail>
-         </div>
+         </div> */}
          <h3 className="closing">We should do this again some time!</h3>
       </h1>
    );
